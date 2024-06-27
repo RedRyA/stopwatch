d07463e663281ce714e7de116b7587a71b29ba69
RedRyA <65385633+RedRyA@users.noreply.github.com>
2024-06-26 19:44:21 -0500

    Add files via upload
    Added excel, will add sql

--- stopwatch.py
+++ stopwatch.py
@@ -1,13 +1,44 @@
-
 import tkinter as tk
 
- 
+import openpyxl
 
 from datetime import datetime, timedelta
-import time
+
+'''''
+
+Possible SQL Server
+
  
 
+SERVER = '99NETWORKING07'
+
+DATABASE = 'Time_Clock'
+
+USERNAME = 'IBC'
+
+PASSWORD =' '
+
+ 
+
+connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};Trusted_Connection=no'
+
+conn = pyodbc.connect(connectionString)
+
+ 
+
+sql = "INSERT INTO dbo.time_store (date, comment) VALUES (%s, %s)"
+
+val=("Monday","Test")
+
+cursor=conn.cursor()
+
+cursor.execute(sql,val)
+
+'''
+
 class Stopwatch:
+
+ 
 
     def __init__(self, master):
 
@@ -18,17 +49,26 @@ class Stopwatch:
         self.start_time = None
 
         self.elapsed_time = 0
-        # array for file
-        self.file_array = []
-        # array for display
-        self.gui_array = []
 
-       
- 
-### MAKE SEPERATE FUNCTIONS FOR PROCESSES
-       
+        self.file_arr = []
+
+        self.gui_arr = []
 
  
+
+       
+
+ 
+
+        global wb
+
+        wb = openpyxl.load_workbook('time_save.xlsx')
+
+        global sheet
+
+        sheet = wb.active
+
+     
 
  # Create UI
 
@@ -36,94 +76,63 @@ class Stopwatch:
 
                              font=("Helvetica", 48))
 
+       
+
+       
+
         self.label.pack()
 
  
-        # create start button
+
         self.start_button = tk.Button(master, text="Start",bg='white', command=self.start)
 
         self.start_button.pack(side="left")
 
  
- # create stop button
+
         self.stop_button = tk.Button(master, text="Stop",bg='white', command=self.stop)
 
         self.stop_button.pack(side="left")
 
  
- # create reset button
+
         self.reset_button = tk.Button(master, text="Reset",bg='white', command=self.reset)
 
         self.reset_button.pack(side="left")
 
  
-# create Times: text
+
         self.time_label = tk.Label(master, text="Times: ")
 
         self.time_label.pack(side="bottom")
 
  
-# create time display box
+
         self.times_listbox = tk.Listbox(master, height=10, width=30)
 
         self.times_listbox.pack(side="bottom")
 
-# create comment text and comment text holder
+ 
 
-        self.comment_label=tk.StringVar()
-        self.comment_label.set("Comment")
-        self.comment_holder=tk.Label(master,textvariable=self.comment_label)
-        self.comment_holder.pack(side='left')
+        self.comment_text=tk.StringVar()
 
+        self.comment_text.set("Comment")
 
-# create comment box
-        self.comment_box=tk.Entry(master)
+        self.comment=tk.Label(master,textvariable=self.comment_text)
 
-        self.comment_box.pack(side="bottom")
+        self.comment.pack(side='left')
 
-# opens and writes to file
-    def file_write(self):
-           
-            global FILE
-            FILE=open(r"time_saver.txt","a")
+ 
 
-            date=datetime.now().strftime('%d:%m:%Y')
+        self.comment_box=tk.Entry(master,width=30)
 
-            FILE.writelines("DATE: "+ date)
+        self.comment_box.pack(side='bottom')
 
-            FILE.writelines('\n')
-            if len(self.comment_box.get())>0:
-                FILE.writelines('Comment: ' )
-                FILE.writelines(self.file_array[0])
-                FILE.writelines('\n')
-                FILE.writelines(self.file_array[1])
-            else:
+       
 
-             FILE.writelines(self.file_array)
+       
 
-             FILE.writelines('\n')
-
-    def array_append(self):
-
-        self.file_array.append(f"{self.comment_box.get()}")
-        self.file_array.append(f"Start: {start_time_str} - Stop: {stop_time_str}")
-           
-
-        self.gui_array.append(f"{self.comment_box.get()}")
-        self.gui_array.append(f"Start: {start_time_str} - Stop: {stop_time_str}")
-
-# clear array and comment box
-    def  clear_arrBox(self):
-        
-            self.comment_box.delete(0,tk.END)
-
-            while len(self.file_array) >0:
-                
-                self.file_array.pop()
-
-            if len(self.gui_array) > 10:
-
-                self.gui_array.pop(0)
+ 
 
     def update(self):
 
@@ -163,6 +172,12 @@ class Stopwatch:
 
             self.update()
 
+       
+
+     
+
+ 
+
  
 
     def stop(self):
@@ -170,43 +185,82 @@ class Stopwatch:
         if self.running:
 
             self.running = False
-        # change color of start and stop buttons when stop is clicked
+
             self.start_button.configure(bg='white')
 
             self.stop_button.configure(bg='red')
 
-            global  start_time_str
-            global  stop_time_str
+ 
 
             start_time_str = self.start_time.strftime('%H:%M:%S')
 
             stop_time_str = datetime.now().strftime('%H:%M:%S')
 
-            self.array_append()
-            self.file_write()
-            self.clear_arrBox()
+ 
 
-        #clear comment box make a seperate function
+           # start_time =  self.file_arr.append(f"{start_time_str}")
 
-            self.comment_box.delete(0,tk.END)
+            #stop_time = self.file_arr.append(f"{stop_time_str}")
 
-            while len(self.file_array) >0:
-                
-                self.file_array.pop()
+            comment_add = f"{self.comment_box.get()}"
 
-            FILE.close()
+           
 
-    
+            self.gui_arr.append(f"{self.comment_box.get()}")
+
+            self.gui_arr.append(f"Start: {start_time_str} - Stop: {stop_time_str}")
+
+         
+
+            #FILE=open(r"time_saver.txt","a")
+
+       
+
+            date=datetime.now().strftime('%d:%m:%Y')
+
+            sheet.append([date,comment_add,start_time_str,stop_time_str])
+
+           # FILE.writelines("DATE: "+ date)
+
+            #FILE.writelines('\n')
+
+            #FILE.writelines( self.file_arr)
+
+            #FILE.writelines('\n')
+
+           
+
+            for time in self.file_arr:
+
+                self.file_arr.pop()
+
+            wb.save('time_save.xlsx')
+
+           # FILE.close()
+
+         
+
+ 
+
+           
+
+            # Keep only the last 10 times
+
+            if len(self.gui_arr) > 10:
+
+                self.gui_arr.pop(0)
+
+           
+
             self.update_times_listbox()
 
-   
-
+ 
 
     def update_times_listbox(self):
 
         self.times_listbox.delete(0, tk.END)
 
-        for time in self.gui_array:
+        for time in self.gui_arr:
 
             self.times_listbox.insert(tk.END, time)
 
@@ -217,22 +271,14 @@ class Stopwatch:
         self.running = False
 
         self.elapsed_time = 0
-       
-        
-       
-        self.label.config(text="00:00:00")
 
-        self.comment_box.delete(0,tk.END)
+        self.label.config(text="00:00:00")
 
         self.start_button.configure(bg='white')
 
         self.stop_button.configure(bg='white')
 
-       
 
-
-    
-        
 
 
 # Create main window
@@ -252,10 +298,3 @@ stopwatch = Stopwatch(root)
 # Run the application
 
 root.mainloop()
-
- 
-
- 
-
-
-
